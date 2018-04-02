c4bd110dc7e1b509fc17a6e8836dbbf8c4ea1cf3
Brian Cooke <brian.cooke@live.co.uk>
2018-04-02 23:07:56 +0100

    Extension questions finished

--- library.rb
+++ library.rb
@@ -8,4 +8,26 @@ class Library
     return @books
   end
 
+  def search_book_title(title)
+    for book in @books
+      return book if book[:title] == title
+    end
+  end
+
+  def return_rental_details(title)
+    for book in @books
+      return book[:rental_details] if book[:title] == title
+    end
+  end
+
+  def add_book_to_list(title)
+    @books.push({title: title, rental_details: {student_name: " ", date: " "}})
+  end
+
+  def change_rental_details(title, student_name, due_date)
+    for book in @books
+        book[:rental_details] = {student_name: student_name, date: due_date} if title == book[:title]
+    end
+  end
+
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -11,30 +11,66 @@ class TestLibrary < MiniTest::Test
           student_name: "Jeff",
           date: "15/02/18"
         }
-      }, {
-        title: "1984",
+      }
+      ])
+    end
+
+    def test_get_books
+      assert_equal([{
+        title: "lord_of_the_rings",
         rental_details: {
-          student_name: "Brian",
-          date: "01/03/18"
+          student_name: "Jeff",
+          date: "15/02/18"
         }
-      }
-    ])
-  end
+        }],
+        @library.get_books)
+      end
 
-  def test_get_books
-    assert_equal([{
-      title: "lord_of_the_rings",
-      rental_details: {
-        student_name: "Jeff",
-        date: "15/02/18"
-      }
-    }, {
-      title: "1984",
-      rental_details: {
-        student_name: "Brian",
-        date: "01/03/18"
-      }
-    }], @library.get_books)
-  end
+      def test_search_book_title
+        assert_equal({
+          title: "lord_of_the_rings",
+          rental_details: {
+            student_name: "Jeff",
+            date: "15/02/18"
+          }
+        },
+        @library.search_book_title("lord_of_the_rings"))
+      end
 
-end
+      def test_return_rental_details
+        assert_equal({
+          student_name: "Jeff",
+          date: "15/02/18"
+        },
+        @library.return_rental_details("lord_of_the_rings"))
+      end
+
+      def test_add_book_to_list
+        assert_equal([{
+          title: "lord_of_the_rings",
+          rental_details: {
+            student_name: "Jeff",
+            date: "15/02/18"
+          }
+          },{
+            title: "1984",
+            rental_details: {
+              student_name: " ",
+              date: " "
+            }
+            }],
+            @library.add_book_to_list("1984"))
+          end
+
+          def test_change_rental_details
+            assert_equal([{
+              title: "lord_of_the_rings",
+              rental_details: {
+                student_name: "Brian",
+                date: "02/04/18"
+              }
+            }],
+            @library.change_rental_details("lord_of_the_rings", "Brian", "02/04/18"))
+          end
+
+        end
